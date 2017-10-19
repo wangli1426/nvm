88bc722a730971c1374eed3283a1719bdaad2e20
Li Wang <wangli1426@gmail.com>
2017-10-19 16:59:10 +0800

    made some optimization to the access io

--- src/accessor/qpair_context.h
+++ src/accessor/qpair_context.h
@@ -9,44 +9,69 @@
 #include <spdk/nvme.h>
 #include "ns_entry.h"
 #include "../utils/sync.h"
+#include "../utils/rdtsc.h"
 
 
 namespace nvm {
+
+    static uint64_t total_cycles = 0;
+
     class QPair {
         struct cb_parameters {
-            cb_parameters(QPair* qp, bool* _is_complete) {
+            cb_parameters(QPair* qp, bool* _is_complete): cb_parameters(qp, _is_complete, 0){};
+            cb_parameters(QPair* qp, bool* _is_complete, uint64_t start_ticks) {
                 qPair = qp;
                 is_complete = _is_complete;
+                start = start_ticks;
             }
             QPair* qPair;
             bool* is_complete;
+            uint64_t start;
         };
     public:
         QPair(struct spdk_nvme_ctrlr *ctrlr, struct spdk_nvme_ns *ns, struct spdk_nvme_qpair *qpair,
-              int queue_length = 8): ctrlr_(ctrlr), ns_(ns), qpair_(qpair), free_slots_(queue_length) {
+              int queue_length = 8): ctrlr_(ctrlr), ns_(ns), qpair_(qpair), free_slots_(queue_length),
+                                     queue_length_(queue_length) {
             sector_size_ = spdk_nvme_ns_get_sector_size(ns);
+            completion_batch_size_ = queue_length / 8;
         }
 
         uint32_t get_sector_size() const {
             return sector_size_;
         }
 
-        synchronous_write(void* content, uint32_t size, uint64_t start_lba) {
+        uint64_t get_sector_number() const {
+            return spdk_nvme_ns_get_num_sectors(ns_);
+        }
+
+        void synchronous_write(void* content, uint32_t size, uint64_t start_lba) {
 
             bool is_complete = false;
-            cb_parameters* cba = new cb_parameters(this, &is_complete);
+            cb_parameters* cba = new cb_parameters(this, &is_complete, ticks());
+            free_slots_ --;
             spdk_nvme_ns_cmd_write(ns_, qpair_, content, start_lba, size / sector_size_, QPair::cb_function,cba, 0);
 
-            while(! is_complete) {
+            while(!is_complete) {
+                process_completions();
+            }
+        }
+
+        void synchronous_read(void* buffer, uint32_t size, uint64_t start_lba) {
+            bool is_complete =  false;
+            cb_parameters* cbp = new cb_parameters(this, &is_complete, ticks());
+            free_slots_ --;
+            spdk_nvme_ns_cmd_read(ns_, qpair_, buffer, start_lba, size / sector_size_, QPair::cb_function, cbp, 0);
+
+            while(!is_complete) {
                 process_completions();
             }
         }
 
         int asynchronous_write(void* content, uint32_t size, uint64_t start_lba, bool *is_complete) {
-            cb_parameters* cba = new cb_parameters(this, is_complete);
+            cb_parameters* cba = new cb_parameters(this, is_complete, ticks());
 
             while(free_slots_ == 0){
-                process_completions();
+                process_completions(completion_batch_size_);
             }
             free_slots_ --;
 
@@ -62,10 +87,10 @@ namespace nvm {
 
         int asynchronous_read(void* buffer, uint32_t size, uint64_t start_lba, bool *is_complete) {
             assert(size > 0 && size % sector_size_ == 0);
-            cb_parameters* cba = new cb_parameters(this, is_complete);
+            cb_parameters* cba = new cb_parameters(this, is_complete, ticks());
 
             while(free_slots_ == 0){
-                process_completions();
+                process_completions(completion_batch_size_);
             }
             free_slots_ --;
 
@@ -79,8 +104,8 @@ namespace nvm {
                                   0);
         }
 
-        inline int process_completions() {
-            spdk_nvme_qpair_process_completions(qpair_, 0);
+        inline int process_completions(uint32_t max_completions = 0) {
+            spdk_nvme_qpair_process_completions(qpair_, max_completions);
         }
 
         void detach() {
@@ -97,6 +122,8 @@ namespace nvm {
             if (para->is_complete) {
                 *para->is_complete = true;
             }
+            total_cycles += ticks() - para->start;
+            delete para;
         }
 
     private:
@@ -104,6 +131,8 @@ namespace nvm {
         struct spdk_nvme_ns	*ns_;
         struct spdk_nvme_qpair *qpair_;
         int free_slots_;
+        uint32_t completion_batch_size_;
+        uint32_t queue_length_;
         uint32_t sector_size_;
     };
 }
--- src/perf.cpp
+++ src/perf.cpp
@@ -10,6 +10,7 @@
 using namespace std;
 using namespace nvm;
 int main() {
+    total_cycles = 0;
     cout << "perf test" << endl;
     int status = nvm::initialize_namespace();
     if (status != 0) {
@@ -17,37 +18,42 @@ int main() {
     } else {
         cout << "namespace is initialized." << endl;
     }
-    QPair *qpair = allocateQPair(8);
+    QPair *qpair = allocateQPair(64);
 
     int number_of_sectors = 1;
-    int number_of_writes = 100000;
+    int number_of_accesses = 100000;
 
     uint32_t sector_size = qpair->get_sector_size();
+    const uint64_t sector_number = qpair->get_sector_number();
 
     uint32_t buffer_size = sector_size * number_of_sectors;
 
-    void* buffer = (char*)spdk_dma_zmalloc(number_of_sectors * qpair->get_sector_size(), 0x1000, NULL);
+    char* buffer = (char*)spdk_dma_zmalloc(number_of_sectors * qpair->get_sector_size(), 0x1000, NULL);
+    snprintf(buffer, 0x1000, "%s", "Hello world!\n");
     bool is_complete = false;
     uint64_t start = ticks();
-    for (int i = 0; i < number_of_writes; i++) {
-//        if (i == number_of_writes - 1)
-//            qpair->asynchronous_write(buffer, buffer_size, rand() % 1024 + (uint64_t)i, &is_complete);
-//        else
-//            qpair->asynchronous_write(buffer, buffer_size, rand() % 1024 + (uint64_t)i, 0);
-        qpair->synchronous_write(buffer, buffer_size, rand() % 1024 + (uint64_t)i);
+    for (int i = 0; i < number_of_accesses; i++) {
+        if (i == number_of_accesses - 1)
+            qpair->asynchronous_read(buffer, buffer_size, rand() % sector_number, &is_complete);
+        else
+            qpair->asynchronous_read(buffer, buffer_size, rand() % sector_number, 0);
+//        qpair->synchronous_write(buffer, buffer_size, rand() % sector_number);
     }
 
 //    while(!is_complete) {
 //        qpair->process_completions();
 //    }
 
+    printf("String: %c\n", buffer[3]);
+
     printf("Number of Free Slots: %d\n", qpair->get_number_of_free_slots());
 
     uint64_t cycles = ticks() - start;
     printf("total cycles: %ld, %ld us per I/O,  %ld IOPS.\n",
            cycles,
-           cycles_to_microseconds(cycles / number_of_writes),
-           1000000000 / cycles_to_nanoseconds(cycles / number_of_writes));
+           cycles_to_microseconds(cycles / number_of_accesses),
+           1000000000 / cycles_to_nanoseconds(cycles / number_of_accesses));
+    printf("complete latency per IO: %ld us\n", cycles_to_microseconds(total_cycles / number_of_accesses));
     spdk_dma_free(buffer);
     qpair->detach();
 }
