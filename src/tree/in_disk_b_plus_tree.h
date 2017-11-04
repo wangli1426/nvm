cbff9ea023afeefe669e3374356341f9b9146208
Li Wang <wangli1426@gmail.com>
2017-11-04 21:40:30 +0800

    can use large block size for the in-disk b+ tree

--- src/tree/in_disk_b_plus_tree.h
+++ src/tree/in_disk_b_plus_tree.h
@@ -12,9 +12,9 @@ namespace tree{
     template<typename K, typename V, int CAPACITY>
     class in_disk_b_plus_tree: public VanillaBPlusTree<K, V, CAPACITY> {
     public:
-        in_disk_b_plus_tree(const char* file_name = "./b_tree.dat"): VanillaBPlusTree<K, V, CAPACITY>(
-                new file_blk_accessor<K, V, CAPACITY>(file_name_, 512)), file_name_(file_name) {
-            set_blk_accessor();
+        in_disk_b_plus_tree(const char* file_name = "./b_tree.dat", const int& block_size = 512): VanillaBPlusTree<K, V, CAPACITY>(
+                new file_blk_accessor<K, V, CAPACITY>(file_name_, block_size)), file_name_(file_name), block_size_(block_size) {
+            set_blk_accessor(block_size_);
         }
 
         virtual ~in_disk_b_plus_tree() {
@@ -22,18 +22,19 @@ namespace tree{
 
         virtual void clear() {
             VanillaBPlusTree<K, V, CAPACITY>::close();
-            set_blk_accessor();
+            set_blk_accessor(block_size_);
             VanillaBPlusTree<K, V, CAPACITY>::init();
         }
     private:
 
-        void set_blk_accessor() {
+        void set_blk_accessor(const int & block_size) {
             if (this->blk_accessor_)
                 delete this->blk_accessor_;
-            this->blk_accessor_ = new file_blk_accessor<K, V, CAPACITY>(file_name_, 512);
+            this->blk_accessor_ = new file_blk_accessor<K, V, CAPACITY>(file_name_, block_size_);
             this->blk_accessor_->open();
         }
         const char* file_name_;
+        const int block_size_;
     };
 }
 #endif //NVM_IN_DISK_B_PLUS_TREE_H
